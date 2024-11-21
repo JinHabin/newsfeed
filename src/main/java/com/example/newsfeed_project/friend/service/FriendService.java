4b03eede660e60b8f2a74a85c4b6de8a4f44d635
Fangsangik <hwangsangik@gmail.com>
2024-11-21 19:07:37 +0900

    refactor : "FriendService" - sendFriendRequest : 친구 요청 보내기 - acceptFriendRequest : 친구 요청 수락 - deleteFriend : 친구 요청 삭제

--- src/main/java/com/example/newsfeed_project/friend/service/FriendService.java
+++ src/main/java/com/example/newsfeed_project/friend/service/FriendService.java
@@ -1,12 +1,17 @@
 package com.example.newsfeed_project.friend.service;
 
+import com.example.newsfeed_project.friend.dto.FriendDto;
 import com.example.newsfeed_project.friend.dto.FriendRequestDto;
 import com.example.newsfeed_project.friend.dto.FriendResponseDto;
+import com.example.newsfeed_project.friend.entity.Friend;
+import com.example.newsfeed_project.member.dto.MemberDto;
+import com.example.newsfeed_project.member.entity.Member;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 public interface FriendService {
-    FriendResponseDto addFriend(FriendRequestDto friendRequestDto);
-    FriendResponseDto acceptFriendApproval(Long requestId, boolean friendApproval);
+    void sendFriendRequest(FriendDto friendDto, String loggedInUserEmail);
+    FriendDto acceptFriendRequest(Long requestId, boolean isApproved, String loggedInUserEmail);
     //List<FriendDto> getFriendList(int page, int size, HttpServletRequest request);
-    void deleteFriend(Long requestId, HttpServletRequest req);
+    void deleteFriend(Long requestId, MemberDto memberDto);
 }
