5a9cae31240abbb5d272c8c07f12ddbcd1d5dcc7
Fangsangik <hwangsangik@gmail.com>
2024-11-21 19:06:05 +0900

    refactor : "FriendRepository" - existsByRequestFriendAndResponseFriend : 요청을 보낸 친구와 받은 친구에 대한 Id - findByIdAndStatus : 친구 Id와 status - findByRequestFriendAndResponseFriend : 요청 친구와 응답 받은 친구가 있는지

--- src/main/java/com/example/newsfeed_project/friend/repository/FriendRepository.java
+++ src/main/java/com/example/newsfeed_project/friend/repository/FriendRepository.java
@@ -1,10 +1,20 @@
 package com.example.newsfeed_project.friend.repository;
 
 import com.example.newsfeed_project.friend.entity.Friend;
+import com.example.newsfeed_project.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    Page<Friend> findByRequestFriendIdAndFriendApproval(Long id, boolean b, Pageable pageable);
+
+    boolean existsByRequestFriendAndResponseFriend(Member requestFriend, Member responseFriend);
+
+    Optional<Friend> findByIdAndStatus(Long id, String status);
+    Optional<Friend> findByRequestFriendAndResponseFriend(Member requestFriend, Member responseFriend);
     //Page<Friend> findByFriendApprovalAndRequestFriendIdOrResponseFriendId(boolean friendApproval, Member requestFriend, Member responseFriend, Pageable pageable);
     //Page<Friend> findFriends(Long memberId, Long friendId , boolean friendApproval, Pageable pageable);
-
 }
