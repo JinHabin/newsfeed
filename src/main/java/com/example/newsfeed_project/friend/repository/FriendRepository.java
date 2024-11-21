6a9ae6d75ece01c161aa1797facb5c5ea84b4f47
Fangsangik <hwangsangik@gmail.com>
2024-11-21 23:48:25 +0900

    refactor : "FriendRepository" - findApprovedFriendsByEmail : email , status 값이 Approve 인 값만 page 처리 - findByRequestFriendIdAndResponseFriendId : Long requestId, Long responseId 값 조회 해서 accept 로직 실행 - findByRequestFriendAndResponseFriend : 요청값을 보낼때 회원에 있는 아이디 값으로 보냄

--- src/main/java/com/example/newsfeed_project/friend/repository/FriendRepository.java
+++ src/main/java/com/example/newsfeed_project/friend/repository/FriendRepository.java
@@ -5,16 +5,19 @@ import com.example.newsfeed_project.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
-    Page<Friend> findByRequestFriendIdAndFriendApproval(Long id, boolean b, Pageable pageable);
+    @Query("SELECT f FROM Friend f WHERE (f.requestFriend.email = :email OR f.responseFriend.email = :email) AND f.status = :status")
+    Page<Friend> findApprovedFriendsByEmail(@Param("email") String email, @Param("status") String status, Pageable pageable);
 
-    boolean existsByRequestFriendAndResponseFriend(Member requestFriend, Member responseFriend);
+    Optional<Friend> findByRequestFriendIdAndResponseFriendId(Long requestId, Long responseId);
 
-    Optional<Friend> findByIdAndStatus(Long id, String status);
+    @Query("SELECT f FROM Friend f WHERE f.id = :requestId AND f.status = :status")
+    Optional<Friend> findByIdAndStatus(@Param("requestId") Long requestId, @Param("status") String status);
+
     Optional<Friend> findByRequestFriendAndResponseFriend(Member requestFriend, Member responseFriend);
-    //Page<Friend> findByFriendApprovalAndRequestFriendIdOrResponseFriendId(boolean friendApproval, Member requestFriend, Member responseFriend, Pageable pageable);
-    //Page<Friend> findFriends(Long memberId, Long friendId , boolean friendApproval, Pageable pageable);
 }
