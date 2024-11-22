4c3605624573d3a84cc05ca0e6ca274528ed6881
habin <habin424@gmail.com>
2024-11-22 17:33:08 +0900

    style : delete requestid

--- newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendDto.java
+++ newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendDto.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class FriendDto {
     private Long id;
-    private Long requestFriendId;
     private Long responseFriendId;
     private String image;
     private String email;
@@ -23,7 +22,6 @@ public class FriendDto {
         if (friend == null || friend.getRequestFriend() == null || friend.getResponseFriend() == null) {
             throw new IllegalArgumentException("Friend 또는 Member 정보가 올바르지 않습니다.");
         }
-        this.requestFriendId = friend.getRequestFriend().getId();
         this.responseFriendId = friend.getResponseFriend().getId();
         this.image = friend.getRequestFriend().getImage(); // 요청자의 이미지
         this.email = friend.getRequestFriend().getEmail(); // 요청자의 이메일
--- newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendResponseDto.java
+++ newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class FriendResponseDto {
+
     private Long requestFriendId;
     private Long responseFriendId;
     private boolean friendApproval;
