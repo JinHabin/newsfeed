7fa8897c4ee22115726729ed62e9098eb3d5245b
habin <habin424@gmail.com>
2024-11-22 17:34:15 +0900

    style : delete no-use package

--- newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendRequestDto.java
+++ newsfeed-main/src/main/java/com/example/newsfeed_project/friend/dto/FriendRequestDto.java
@@ -2,11 +2,11 @@ package com.example.newsfeed_project.friend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import jakarta.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 public class FriendRequestDto {
+
     private Long requestFriendId;
     private Long responseFriendId;
 
